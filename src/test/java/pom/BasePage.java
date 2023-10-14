f40d25476c79c9becd2944aee2038ebbd241d955
SuvorovaAnna <anna.suvorova@testpro.io>
2023-10-13 23:00:12 -0600

    Create BasePage.java

---
+++ src/test/java/pom/BasePage.java
@@ -0,0 +1,37 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.time.Duration;
+
+public class BasePage {
+
+    private WebDriver driver;
+    Actions actions;
+    Wait<WebDriver> wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        actions = new Actions(driver);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void clickToElement(WebElement element) {
+        element.click();
+    }
+
+    public void sendKeysToElement(WebElement element, String text) {
+        element.sendKeys(text);
+    }
+
+    public WebElement findWebElement(By locator) {
+       return driver.findElement(locator);
+    }
+
+}
