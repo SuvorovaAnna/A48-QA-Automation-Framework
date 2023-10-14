a16df3f51b8fb0abb30098b66ad5c3957c75f281
SuvorovaAnna <anna.suvorova@testpro.io>
2023-10-13 23:00:21 -0600

    Create LoginPage.java

---
+++ src/test/java/pom/LoginPage.java
@@ -0,0 +1,30 @@
+package pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = findWebElement(By.cssSelector("input[type='email']"));
+        clickToElement(emailField);
+        sendKeysToElement(emailField, email);
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = findWebElement(By.cssSelector("input[type='password']"));
+        clickToElement(passwordField);
+        sendKeysToElement(passwordField, password);
+    }
+
+    public void clickLoginButton(){
+        WebElement loginButton = findWebElement(By.cssSelector("button[type='submit']"));
+        clickToElement(loginButton);
+    }
+
+
+}
