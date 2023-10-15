feaaaae9fecc50732fb724dcae57fb8975b64eac
SuvorovaAnna <anna.suvorova@testpro.io>
2023-10-14 23:31:27 -0600

    Update Homework22.java

--- src/test/java/Homework22.java
+++ src/test/java/Homework22.java
@@ -4,34 +4,28 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pom.HomePage;
 import pom.LoginPage;
 
 public class Homework22 extends BaseTest{
     LoginPage loginPage;
+    HomePage homePage;
 
-    public static final String NEW_PLAYLIST_NAME = "New Name";
     @Test(dataProvider ="loginParameters", dataProviderClass = DataProviderClass.class)
     public void renamePlaylist(String email, String password){
 
         loginPage = new LoginPage(getDriver());
+        homePage = new HomePage(getDriver());
 
-        loginPage.provideEmail(email);
-        loginPage.providePassword(password);
-        loginPage.clickLoginButton();
+        loginPage.provideEmail(email)
+                .providePassword(password)
+                .clickLoginButton();
 
-        wait.until(ExpectedConditions.visibilityOf(loginPage.findWebElement(By.cssSelector("button[type='submit']"))));
+        loginPage.waitUntilLoginButtonDisplayed();
 
-        WebElement playlist = getDriver().findElement(By.cssSelector(".playlist:nth-child(3)"));
-        actions.doubleClick(playlist).perform();
+        homePage.playlist();
+        homePage.setPlaylistInputField();
 
-        WebElement playlistInputField = getDriver().findElement(By.cssSelector("[name='name']"));
-        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "New Name", Keys.BACK_SPACE));
-
-        playlistInputField.sendKeys(NEW_PLAYLIST_NAME);
-        playlistInputField.sendKeys(Keys.ENTER);
-
-        WebElement notificationRenamePlaylist = getDriver().findElement(By.cssSelector("div.success.show"));
-        Assert.assertTrue(notificationRenamePlaylist.isDisplayed());
-
+        Assert.assertTrue(homePage.isNotificationRenamePlaylistDisplayed());
     }
 }
